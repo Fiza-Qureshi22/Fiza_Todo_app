2dcb0f426c35c285cc258099e95b614f93b5593e
Fizy_Fizu <163686458+Fiza-Qureshi22@users.noreply.github.com>
2024-06-10 22:47:26 +0500

    no changes
    "Added a CLI-based to-do app for efficient task management."

---
+++ main.js
@@ -0,0 +1,67 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// todos array
+// todos function
+//todos operation
+let todos = ["Fiza", "Habiba", "abuBakar", "umarFarooq"];
+async function creatTodo(todos) {
+    do {
+        let ans = await inquirer.prompt([
+            {
+                name: "select",
+                type: "list",
+                message: "select an operation",
+                choices: ["add", "update", "view", "delet"]
+            }
+        ]);
+        if (ans.select === "add") {
+            let addTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "input",
+                    message: "Add Item",
+                }
+            ]);
+            todos.push(addTodo.todo);
+            console.log(todos);
+        }
+        ;
+        if (ans.select === "update") {
+            let updateTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "list",
+                    message: "select an Item for the update",
+                    choices: todos.map(item => item)
+                }
+            ]);
+            let addTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "input",
+                    message: "Add Item",
+                }
+            ]);
+            let newTodos = todos.filter(val => val !== updateTodo.todo);
+            todos = [...newTodos, addTodo.todo];
+            console.log(todos);
+        }
+        if (ans.select === "view") {
+            console.log(todos);
+        }
+        if (ans.select === "delet") {
+            let deletTodo = await inquirer.prompt([
+                {
+                    name: "todo",
+                    type: "list",
+                    message: "select an Item for the update",
+                    choices: todos.map(item => item)
+                }
+            ]);
+            let newTodos = todos.filter(val => val !== deletTodo.todo);
+            todos = [...newTodos];
+            console.log(todos);
+        }
+    } while (true);
+}
+creatTodo(todos);
